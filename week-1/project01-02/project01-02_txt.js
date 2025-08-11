829e50ce263dac71281ecc8015c5c4ec7950d188
Shannon Kueneke <slkueneke@gmail.com>
2025-08-11 11:44:25 -0700

    week 1 hands-on project

--- week-1/project01-02/project01-02_txt.js
+++ week-1/project01-02/project01-02_txt.js
@@ -2,10 +2,11 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Shannon Kueneke
+    Date: August 11, 2025
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate", service1Speed = "50 Mbsp", service2Speed = "100 Mbsp", service3Speed = "500 Mbsp", service4Speed = "1 Gig";
