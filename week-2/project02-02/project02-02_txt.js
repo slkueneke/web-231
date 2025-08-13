86903ecd3880a8507ef01b4d1983c7d7c9f43792
Shannon Kueneke <slkueneke@gmail.com>
2025-08-13 14:22:22 -0700

    week 2 project

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -3,9 +3,21 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Shannon Kueneke
+      Date: August 13, 2025
 
       Filename: project02-02.js
  */
- 
+
+function verifyForm() {
+  //get form field values
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+
+  //check if all fields contain a value/are not blank
+  (name && email && phone) ? window.alert("Thank you!") : window.alert("Please fill in all fields");
+}
+
+document.getElementById("submit").onclick = verifyForm;
+
