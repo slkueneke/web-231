4e08fdfb9769c2ba8a7ffe7c471be73c0e950108
Shannon Kueneke <slkueneke@gmail.com>
2025-08-19 13:06:25 -0700

    corrections to week 1

--- week-1/project01-02/project01-02_txt.js
+++ week-1/project01-02/project01-02_txt.js
@@ -9,4 +9,4 @@
 */
 
 //define variables for service name and service speed
-service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate", service1Speed = "50 Mbsp", service2Speed = "100 Mbsp", service3Speed = "500 Mbsp", service4Speed = "1 Gig";
+var service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate", service1Speed = "50 Mbsp", service2Speed = "100 Mbsp", service3Speed = "500 Mbsp", service4Speed = "1 Gig";
