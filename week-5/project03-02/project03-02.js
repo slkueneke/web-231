57bb0406a302f9ee00409786798a3141c30dacbe
Shannon Kueneke <slkueneke@gmail.com>
2025-09-09 15:11:12 -0700

    week 5 assignment

--- week-5/project03-02/project03-02.js
+++ week-5/project03-02/project03-02.js
@@ -3,8 +3,8 @@
       Project 03-02
 
       Application to generate a slide gallery
-      Author: 
-      Date:   
+      Author: Shannon Kueneke
+      Date: 9/9/2025
 
       Filename: project03-02.js
 */
@@ -12,9 +12,9 @@
 let captions = new Array(14);
 captions[0]="International Space Station fourth expansion [2009]";
 captions[1]="Assembling the International Space Station [1998]";
-captions[2]="The Atlantis docks with the ISS [2001]"; 
-captions[3]="The Atlantis approaches the ISS [2000]"; 
-captions[4]="The Atlantis approaches the ISS [2000]"; 
+captions[2]="The Atlantis docks with the ISS [2001]";
+captions[3]="The Atlantis approaches the ISS [2000]";
+captions[4]="The Atlantis approaches the ISS [2000]";
 captions[5]="International Space Station over Earth [2002]";
 captions[6]="The International Space Station first expansion [2002]";
 captions[7]="Hurricane Ivan from the ISS [2008]";
@@ -25,4 +25,11 @@ captions[11]="The International Space Station second expansion [2006]";
 captions[12]="The International Space Station third expansion [2007]";
 captions[13]="The ISS over the Ionian Sea [2007]";
 
+
+var htmlCode = "";
+for (var i = 0; i<captions.length; i++) {
+  htmlCode += "<figure><img alt='' src='slide" + i + ".jpg' /><figcaption>" + captions[i] + "</figcaption></figure>";
+}
+
+document.getElementById("gallery").innerHTML = htmlCode;
 
