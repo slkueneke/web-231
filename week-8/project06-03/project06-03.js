8b5870b2b7286d0547e6724314eac22c928c8033
Shannon Kueneke <slkueneke@gmail.com>
2025-10-01 08:32:35 -0700

    week 8 assignments

---
+++ week-8/project06-03/project06-03.js
@@ -0,0 +1,56 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-03
+
+      Script to complete a form containing billing and shipping address information
+      Author: Shannon Kueneke
+      Date: September 30, 2025
+
+      Filename: project06-03.js
+*/
+"use strict";
+
+let useShip = document.getElementById("useShip");
+useShip.addEventListener("click", copyShippingToBilling);
+
+//Copies shipping fields to corresponding billing fields
+function copyShippingToBilling() {
+  //if useShip is checked
+  if (useShip.checked) {
+    //copy values for all text inputs
+    /*let shippingInputs = document.querySelectorAll("#shipping input[type='text']");
+    let billingInputs = document.querySelectorAll("#billing input[type='text']");
+    for (let i=0; i<shippingInputs.length; i++) {
+      billingInputs[i].value = shippingInputs[i].value;
+    }*/
+
+    document.billShip.elements.firstnameBill.value = document.billShip.elements.firstnameShip.value;
+    document.billShip.elements.lastnameBill.value = document.billShip.elements.lastnameShip.value;
+    document.billShip.elements.address1Bill.value = document.billShip.elements.address1Ship.value;
+    document.billShip.elements.address2Bill.value = document.billShip.elements.address2Ship.value;
+    document.billShip.elements.cityBill.value = document.billShip.elements.cityShip.value;
+    document.billShip.elements.countryBill.value = document.billShip.elements.countryShip.value;
+    document.billShip.elements.codeBill.value = document.billShip.elements.codeShip.value;
+
+    //copy value of ship state select to billing state select value
+    let stateShip = document.getElementById("stateShip");
+    let stateBill = document.getElementById("stateBill");
+    stateBill.value = stateBill[stateShip.selectedIndex].value;
+  }
+}
+
+//Add validation error to all text input fields
+let formElements = document.querySelectorAll("input[type='text']");
+let fieldCount = formElements.length;
+let errorBox = document.getElementById("errorBox");
+for (let i=0; i<fieldCount; i++) {
+  formElements[i].addEventListener("invalid", showValidationError);
+}
+
+//Function to override native error messaging and display custom error in #errorBox
+function showValidationError(evt) {
+  evt.preventDefault();
+  errorBox.textContent = "Complete all highlighted fields"
+}
+
